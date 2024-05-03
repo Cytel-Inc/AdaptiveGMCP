b25b6ef2a0b8416e6517eada14a68256cc6aa8e9
Ajoy Mukhopadhyay <ajoy.mukhopadhyay@cytel.com>
2024-05-03 10:50:13 +0530

    Bug fix in sample size integerization

--- R/cerStage1Analysis.R
+++ R/cerStage1Analysis.R
@@ -86,34 +86,3 @@ PerformStage1Test <- function(
 
   list("Stage1Tables" = Stage1Tables, "Stage1Obj" = Stage1Obj)
 }
-
-
-
-
-
-# Planned Arm-wise & Look-wise samples------------------------- -
-getPlanAllocatedSamples <- function(SS, allocRatio, info_frac) {
-  col_names <- c("Control", paste("Treatment", 1:(length(allocRatio) - 1), sep = ""))
-  ss_frac <- allocRatio / sum(allocRatio)
-  ss_lk <- round(SS * info_frac)
-  ss_lk[length(info_frac)] <- SS
-  ss_incr <- c(ss_lk[1], diff(ss_lk))
-  ss_plan_incr <- t(sapply(ss_incr, function(x) round(x * ss_frac)))
-
-  if (length(info_frac) > 1) ss_plan_incr[, 1] <- ss_incr - rowSums(ss_plan_incr[, -1])
-
-  ss_plan_incr <- data.frame(ss_plan_incr,row.names = NULL)
-  colnames(ss_plan_incr) <- col_names
-
-  # ss_plan_cum <- matrix(NA, nrow = nrow(ss_plan_incr), ncol = ncol(ss_plan_incr))
-  ss_plan_cum <- data.frame(matrix(NA, nrow = nrow(ss_plan_incr), ncol = ncol(ss_plan_incr)))
-  for (i in 1:nrow(ss_plan_incr)) {
-    if (i == 1) {
-      ss_plan_cum[i, ] <- ss_plan_incr[i, ]
-    } else {
-      ss_plan_cum[i, ] <- ss_plan_incr[i, ] + ss_plan_cum[i - 1, ]
-    }
-  }
-  colnames(ss_plan_cum) <- col_names
-  list("IncrementalSamples" = ss_plan_incr, "CumulativeSamples" = ss_plan_cum)
-}
